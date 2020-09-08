0740f561d64a6b010d97dbc5c7656215a73e3cbe
balabin Anton <aa7224@gmail.com>
2020-09-09 02:55:47 +0300

    Start fighting with collision detection

--- Assets/Scripts/PlayerScripts/ThirdPersonCamera.cs
+++ Assets/Scripts/PlayerScripts/ThirdPersonCamera.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
@@ -8,36 +13,46 @@ public class ThirdPersonCamera : MonoBehaviour
     public float cameraSpeed = 3;
 
     private Vector3 m_currentCoordinates = Vector3.zero;
-    private Vector3 m_futureLocation = Vector3.zero;
     private float m_distance = 1.0f;
+
+    private void Awake()
+    {
+        m_distance = Vector3.Distance(transform.parent.position, transform.position);
+    }
 
     private void Update()
     {
         CameraControl();
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawSphere(m_futureLocation, 0.3f);
-        Debug.DrawRay(transform.parent.position, m_futureLocation, Color.blue);
-    }
-
     private void CameraControl()
     {
         if (m_currentCoordinates != Vector3.zero)
         {
-            m_futureLocation = transform.position + m_currentCoordinates.normalized * (Time.deltaTime * cameraSpeed);
             RaycastHit hit;
-
-            if (Physics.Raycast(transform.parent.position, m_futureLocation, out hit))
+            
+            Debug.Log($"{m_distance}");
+            Debug.DrawRay(transform.parent.position, transform.localPosition.normalized, Color.blue);
+            if (Physics.Raycast(transform.parent.position, transform.localPosition.normalized, out hit, m_distance))
             {
-                m_distance = Mathf.Clamp(hit.distance, 1.0f, 3.0f);
+                if (m_distance > 1.0f)
+                {
+                    transform.position = hit.point;
+                    transform.Translate(transform.forward * Time.deltaTime);
+                }
             }
             else
             {
-                m_distance = 3.0f;
+                if (m_distance < 4.0f)
+                {
+                    transform.Translate(-transform.forward * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(transform.forward * Time.deltaTime);
+                }
             }
+            m_distance = Vector3.Distance(transform.parent.position, transform.localPosition);
             transform.Translate(m_currentCoordinates.normalized * (Time.deltaTime * cameraSpeed));
             transform.LookAt(player.position);
         }
